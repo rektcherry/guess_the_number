5a0cab848ca933a814af0c2ebe193b2017457756
rektcherry <100161898+rektcherry@users.noreply.github.com>
2022-03-05 00:30:07 +0200

    Create playme.py

---
+++ playme.py
@@ -0,0 +1,14 @@
+import random 
+num = random.randint(0,10)
+
+guess1 = input('Guess what number between 0 - 10 I\'m thinking. You get 5 tries go: ')
+wrongAns = ['You may try again, but it looks hopeless','Odds are never in your favour. Try again.','Wrong','Almost there! Or are you?','Not even close!']
+righAns = 'Wow, yaarrr a winner Harrry! Such winning, I take the L.'
+randAns = secrets.choise(wrongAns)
+maxRound = 5
+round = 0
+
+while round < maxRound:
+     round += 1
+     if 
+#### continues ####
