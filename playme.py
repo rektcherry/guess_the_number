8621ee99b3cb91d57dce19334d07e7cf95e4c1e3
rektcherry <100161898+rektcherry@users.noreply.github.com>
2022-03-05 10:45:37 +0200

    Update playme.py

--- playme.py
+++ playme.py
@@ -1,14 +1,30 @@
-import random 
-num = random.randint(0,10)
-
-guess1 = input('Guess what number between 0 - 10 I\'m thinking. You get 5 tries go: ')
+import random
+import sys
+guess = input('Guess what number between 0 - 10 I\'m thinking. You get 5 tries go: ')
 wrongAns = ['You may try again, but it looks hopeless','Odds are never in your favour. Try again.','Wrong','Almost there! Or are you?','Not even close!']
 righAns = 'Wow, yaarrr a winner Harrry! Such winning, I take the L.'
-randAns = secrets.choise(wrongAns)
 maxRound = 5
 round = 0
 
-while round < maxRound:
-     round += 1
-     if 
-#### continues ####
+def main ():
+    while round < maxRound:
+         num = random.randint(0,10)
+         randAns = secrets.choise(wrongAns)
+         round += 1
+         if guess == num:
+             print(rightAns)
+             break
+         elif guess != num:
+             print(randAns + '/n')
+             guess = input('Round ' + round + '. Go: ')
+             continue
+         if round == maxRound:
+             print('Thanks for playing.')
+             playAgain = input('Play again type 1. Exit type 0: ')
+                 if playAgain == 1:
+                     round = 0
+                 else:
+                     sys.exit('bye bye')
+
+main()
+
